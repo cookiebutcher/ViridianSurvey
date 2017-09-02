01da979977e932aa2b5d17bfe1ee3a61099f4b6a
Cookie Butcher <cookie.butcher@gmail.com>
2017-09-02 14:55:57 +0200

    survey component changes

--- src/ViridianSurveyWebApp/ClientApp/app/dataservices/survey.service.ts
+++ src/ViridianSurveyWebApp/ClientApp/app/dataservices/survey.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-//import 'rxjs/add/operator/map';
-//import 'rxjs/add/operator/catch';
 import { ISurvey } from '../datamodels/survey.model';
 
 @Injectable()
